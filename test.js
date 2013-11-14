1638917dc0809811a4587ab3d7a0a69df1aa8d54
Lee Goddard <lee@leegoddard.net>
2013-11-14 10:42:31 +0100

    Extended test for varied req methods. Export default server port

--- test.js
+++ test.js
@@ -24,8 +24,15 @@ var dbConfig = {
 
 var APP; // The server instance under test.
 
-function testGet (uri, callback){
-	http.request(uri, function(res) {
+
+function testURI (method, path, next){
+	console.log('---->'+method+' '+path);
+	http.request({
+		hostname:	'localhost',
+		port:		Server.Server.DEFAULT_PORT,
+		method:		method,
+		path:		path
+	}, function(res) {
 		res.setEncoding('utf8');
 	})
 	.on('response', function (res) {
@@ -36,7 +43,7 @@ function testGet (uri, callback){
 		});
 		res.on('end', function(){
 			rawBody.should.be.type('string');
-			callback( JSON.parse(rawBody), res );
+			next( JSON.parse(rawBody), res );
 		});
 	})
 	.end();
@@ -140,14 +147,9 @@ describe('model', function(){
 	});
 });
 
-describe('app', function(){
-	var model = new Model.Model(
-		dbConfig,
-		new View.View
-	);
-
-	it('should be defined with an infline function', function (done) {
-		model.should.be.type('object');
+describe('app config', function(){
+	it('should have a default port of 3000', function(done){
+		Server.Server.DEFAULT_PORT.should.equal(3000);
 		done();
 	});
 });
@@ -179,15 +181,21 @@ describe('URIs', function(){
 		done();
 	});
 
-	it('should be listening at localhost:3333', function (done) {
+	it('should be listening at localhost:3333 via http.get method', function (done) {
 		http.get('http://localhost:3000/', function (res) {
+			res.statusCode.should.be.equal(200);
+			done();
+		});
+	});
+	it('should be listening at localhost:3333 via test func', function (done) {
+		testURI('GET','/', function (body, res) {
 			res.statusCode.should.be.equal(200);
 			done();
 		});
 	});
 
 	it('should error for a non-existant table', function (done) {
-		testGet('http://localhost:3000/Notable'+(new Date().getTime()), function (body,res) {
+		testURI('GET','/Notable'+(new Date().getTime()), function (body,res) {
 			res.statusCode.should.be.equal(200);
 			body.should.have.property('error');
 			body.error.should.have.keys('errno', 'code', 'sqlState', 'index');
@@ -213,7 +221,7 @@ describe('URIs', function(){
 
 	it('should return wrapped results for an existant table, '+TEST_DATABASE+'.'+TEST_TABLE,
 		function (done) {
-			testGet('http://localhost:3000/'+TEST_TABLE, function (body) {
+			testURI('GET','/'+TEST_TABLE, function (body) {
 				body.should.not.have.property('error');
 				body.should.have.property('results');
 				body.results.length.ok;
@@ -224,7 +232,7 @@ describe('URIs', function(){
 
 	it('should select by column', function (done) {
 		var columnName = 'text';
-		testGet('http://localhost:3000/'+TEST_TABLE+'/'+columnName, function(body){
+		testURI('GET','/'+TEST_TABLE+'/'+columnName, function(body){
 			body.should.not.have.property('error');
 			body.should.have.property('results');
 			body.results.length.ok;
@@ -234,20 +242,29 @@ describe('URIs', function(){
 		});
 	});
 
+	var existingRecord;
 	it('should select by column and column value', function (done) {
 		var columnName = 'id';
 		var columnValue = 1;
-		testGet('http://localhost:3000/'+TEST_TABLE+'/'+columnName+'/'+columnValue, function(body){
+		testURI('GET','/'+TEST_TABLE+'/'+columnName+'/'+columnValue, function(body){
 			body.should.not.have.property('error');
 			body.should.have.property('results');
 			body.results.length.ok;
 			body.results[0].should.be.an.instanceOf( Object );
+			body.results[0].should.have.property('id');
+			body.results[0].should.have.property('text');
+			existingRecord = body.results[0];
 			done();
 		});
 	});
 
 	it('should select by two column and column value', function (done) {
-		testGet('http://localhost:3000/'+TEST_TABLE+'/id/1/id/1', function(body){
+		existingRecord.should.be.an.instanceOf( Object );
+		var path = '/'+TEST_TABLE
+			+ '/id/' + existingRecord.id
+			+ '/text/' + encodeURI( existingRecord.text );
+
+		testURI('GET', path, function(body){
 			body.should.not.have.property('error');
 			body.should.have.property('results');
 			body.results.length.ok;
@@ -256,5 +273,26 @@ describe('URIs', function(){
 		});
 	});
 
-});
+	/*
+	it('should delete by column value', function (done) {
+		existingRecord.should.be.an.instanceOf( Object );
+		var uri = 'http://localhost:3000/'+TEST_TABLE
+			+ '/id/' + existingRecord.id;
 
+		testGet(uri, function(body){
+			body.should.not.have.property('error');
+			body.should.have.property('results');
+			body.results.length.ok;
+			body.results[0].should.be.an.instanceOf( Object );
+			done();
+		});
+
+		testGet(uri, function(body){
+			body.should.have.property('error');
+			done();
+		});
+	});
+
+	*/
+
+});
